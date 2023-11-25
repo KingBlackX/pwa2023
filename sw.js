9d75c9cb07d36d10110104423e5ec9f53d1f9fc6
KingBlackX <151684651+KingBlackX@users.noreply.github.com>
2023-11-25 12:57:16 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,48 +1,52 @@
 // Importar la CDN de workbox
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
-);
-// Verificamos si la variable workbox esta definida y disponible
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+
+// Verificamos si la variable workbox está definida y disponible
 if (workbox) {
-  console.log("workbox esta cargado.");
+  console.log("workbox está cargado");
   workbox.precaching.precacheAndRoute([]);
-  // Cahe de imagenes en la carpeta public/img
-  workbox.routing.registerRoute(/(.*)others(.*)\.(?:png|gif|jpg)/, new workbox.strategies.CacheFirst({
-    cacheName: "images",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 50,
-        maxAgeSeconds: 30 * 24 * 60 * 60
-      })
-    ]
-  }));
-  // Hacemos que el contenido en js y css sean rapidos devolviendo los assets de la cache
+
+  // Cache de imágenes en la carpeta public/img
   workbox.routing.registerRoute(
-    // Cache de js, css y archivos de CND
-    /.*\.(?:css|js|scss|)/,
-    // Usamos el cache para actualizar en segundo plano
+    /.*\/img\/(.*)\.(?:png|gif|jpg)/,
+    new workbox.strategies.CacheFirst({
+      cacheName: "images",
+      plugins: [
+        new workbox.expiration.ExpirationPlugin({
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60
+        })
+      ]
+    })
+  );
+
+  // Caché de archivos JS, CSS y otros
+  workbox.routing.registerRoute(
+    /\.(?:css|js|scss)$/,
     new workbox.strategies.StaleWhileRevalidate({
       cacheName: "assets",
     })
   );
-  // Cache de fuentes de Google
+
+  // Caché de fuentes de Google
   workbox.routing.registerRoute(
     new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
     new workbox.strategies.CacheFirst({
       cacheName: "google-fonts",
       plugins: [
-        new workbox.cacheableResponse.Plugin({
+        new workbox.cacheableResponse.CacheableResponsePlugin({
           statuses: [0, 200],
         }),
       ],
     })
   );
-  // Agregar analisis Offline
+
+  // Agregar análisis offline
   workbox.googleAnalytics.initialize();
 
   // Instalar un nuevo service worker
   workbox.core.skipWaiting();
   workbox.core.clientsClaim();
-}else{
-  console.log("Workbox Fallo");
+} else {
+  console.log("Workbox falló");
 }
